ca067bd3dde5c61cca0e8dc65d21d3fabb5c50ef
52pig <1478017615@qq.com>
2018-11-15 20:53:18 +0800

    'add_cut_word'

--- learn/neural_dual_entailment/encoder.py
+++ learn/neural_dual_entailment/encoder.py
@@ -1,16 +1,17 @@
-#-*- coding: UTF-8 -*-
-import jieba
+# -*- coding: UTF-8 -*-
 
+import jieba
+import codecs
 
 def encode_sentence(filename):
     user_dict = 'D:/BaiduNetdiskDownload/dataset_dir/user_dict.txt'
     jieba.load_userdict(user_dict)
     row = 0
+    out_file = codecs.open('D:/BaiduNetdiskDownload/dataset_dir/out/out_cut_word.txt', "w", encoding='UTF-8')
     for k, v in enumerate(open(filename, encoding='UTF-8')):
         lines = v.strip().split('\t')
         question = lines[0].strip()
         answer = lines[1].strip()
-
 
         cut_question = jieba.cut(question, cut_all=False)
         cut_answer = jieba.cut(answer, cut_all=False)
@@ -25,7 +26,10 @@ def encode_sentence(filename):
             for proc_a in cut_answer:
                 proced_answer_list.append(proc_a)
             proced_answer_str = ' '.join(proced_answer_list)
-            print(proced_question_str, ':::', proced_answer_str)
+            #print(proced_question_str, ':::', proced_answer_str)
+            proced_line = '\t'.join((proced_question_str, proced_answer_str))
+
+            out_file.write(proced_line+"\n")
             # print(proced_question, proced_answer)
             #print(v.strip())
             row += 1
